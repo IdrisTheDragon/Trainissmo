c74a298cdba6e5035a236fb1d7b1fd5b06d51629
Nathan <nathaniwilliamsuk@gmail.com>
2021-06-09 16:17:26 +0100

    move on innit code so everything actually works

--- control.lua
+++ control.lua
@@ -2,17 +2,3 @@ require 'utils.data_stages'
 _LIFECYCLE = _STAGE.control -- Control stage
 
 require 'modules.immersive_cargo_wagons.main'
-
-function OnInit()
-    local ship_items = remote.call("freeplay", "get_ship_items")
-    ship_items['locomotive'] = (ship_items['locomotive'] or 0) + 1
-    ship_items['cargo-wagon'] = (ship_items['cargo-wagon'] or 0) + 1
-
-    local debris_items = remote.call("freeplay", "get_debris_items")
-    debris_items['rail'] = (debris_items['rail'] or 0) + 25
-    
-    remote.call("freeplay", "set_ship_items", ship_items)
-    remote.call("freeplay", "set_debris_items", debris_items)
-end
-
-script.on_init(OnInit)
--- modules/immersive_cargo_wagons/main.lua
+++ modules/immersive_cargo_wagons/main.lua
@@ -175,6 +175,16 @@ end
 
 local function on_init()
     Public.reset()
+
+    local ship_items = remote.call("freeplay", "get_ship_items")
+    ship_items['locomotive'] = (ship_items['locomotive'] or 0) + 1
+    ship_items['cargo-wagon'] = (ship_items['cargo-wagon'] or 0) + 1
+
+    local debris_items = remote.call("freeplay", "get_debris_items")
+    debris_items['rail'] = (debris_items['rail'] or 0) + 25
+    
+    remote.call("freeplay", "set_ship_items", ship_items)
+    remote.call("freeplay", "set_debris_items", debris_items)
 end
 
 function Public.get_table()
